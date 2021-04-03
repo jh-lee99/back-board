81ed9cce25c94a398e07f497549111f68bb77bae
eastflag <eastflag@gmail.com>
2021-04-03 16:45:13 +0900

    json 데이터 보내고 받기

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 
 let app = express();
 
+// body-parser는 내장되어있음.  json 파싱하기 위해서 설정만 추가
+app.use(express.json());
+
 // express에는 json 데이터를 파싱하는 모듈이 내장되어있다.
 // 하지만 json만 되고 x-www-form-urlencoded를 파싱하기 위해서 아래를 확장해야 한다.
 app.use(express.urlencoded({
@@ -40,6 +43,21 @@ app.post('/hello33', (req, res) => {
 app.post('/hello4', (req, res) => {
   const {name} = req.body;
   res.send(`Hello ${name}`);
+})
+
+// response - json 데이터 보내기
+app.post('/hello5', (req, res) => {
+  const result = {
+    code: 0,
+    message: 'success'
+  };
+  res.send(result);
+})
+
+// request - json 데이터 받기
+app.post('/hello6', (req, res) => {
+  const result = req.body;
+  res.send(result);
 })
 
 app.listen(8080, () => {
